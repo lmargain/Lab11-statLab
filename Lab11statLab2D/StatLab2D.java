f8770058e6a52e692ee7f60d4631c93523bb843e
lmargain <787105@seq.org>
2016-09-27 08:45:15 -0700

    Add files via upload

---
+++ Lab11statLab2D/StatLab2D.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class StatLab2D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StatLab2D
+{
+
+
+    /**
+     * Constructor for objects of class StatLab2D
+     */
+    public StatLab2D()
+    {
+       
+    }
+
+}
---
+++ Lab11statLab2D/statLab2DRunner.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class statLab here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class statLab2DRunner
+{
+    public static void main(String args[]){
+        int rows = 5;
+        int cols = 10;
+        int[][] nums = new int[rows][cols];
+        StatLab2D statLab2D = new StatLab2D(nums);
+        
+    }       
+}
